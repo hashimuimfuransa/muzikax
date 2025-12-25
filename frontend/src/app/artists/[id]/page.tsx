cc2724ee09861db21b9997ac61662338090d9dda
tuy250 <dieudonnetuy250@gmail.com>
2025-12-25 09:36:35 +0200

    init

--- frontend/src/app/artists/[id]/page.tsx
+++ frontend/src/app/artists/[id]/page.tsx
@@ -351,12 +351,12 @@ export default function ArtistProfilePage() {
                       await followCreator(creator._id);
                       
                       // Update the followers count in the UI
-                      if (prev) {
-                        setCreator({
+                      setCreator(prev => 
+                        prev ? {
                           ...prev,
                           followersCount: prev.followersCount + 1
-                        });
-                      }
+                        } : null
+                      );
                       
                       // Show success feedback
                       console.log('Successfully followed creator');
